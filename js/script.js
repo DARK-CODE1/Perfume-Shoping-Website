fab1218c1d7325d2ae2ce87fbd88fc3ed32ef28d
DARK-CODE1 <hashemy606@gmail.com>
2024-07-29 21:56:55 +0430

    add css for mobile menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-$(function () {
-    
-});
+ $(function () {
+    'use strict';
+
+    // canvas menu activasion
+
+    $('.canvas-open').on('click', function () {
+        $('.off-canvas-menu-wrapper,.off-canvas-overlay').addClass('active');
+    });
+
+    $('.canvas-open').on('click', function () {
+        $('.off-canvas-menu-wrapper,.off-canvas-overlay').removeClass('active');
+    });
+ });
