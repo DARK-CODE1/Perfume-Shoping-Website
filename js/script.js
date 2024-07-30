a902ef8f6a8912ea1e9a7e881efb897b0db159a8
DARK-CODE1 <hashemy606@gmail.com>
2024-07-31 03:24:18 +0430

    update icon and css

--- js/script.js
+++ js/script.js
@@ -1,13 +1,49 @@
- $(function () {
-    'use strict';
+$(function () {
+  'use strict';
 
-    // canvas menu activasion
+  // canvas menu activasion
 
-    $('.canvas-open').on('click', function () {
-        $('.off-canvas-menu-wrapper,.off-canvas-overlay').addClass('active');
-    });
+  $(".canvas_open").on("click", function () {
+    $(".offcanvas_menu_wrapper,.offcanvas_overlay").addClass("active");
+  });
 
-    $('.canvas-open').on('click', function () {
-        $('.off-canvas-menu-wrapper,.off-canvas-overlay').removeClass('active');
-    });
- });
+  $(".canvas_close,.offcanvas_overlay").on("click", function () {
+    $(".offcanvas_menu_wrapper,.offcanvas_overlay").removeClass("active");
+  });
+
+  //  offcanvas menu
+  let $offcanvasNav = $('.offcanvas_main_menu'),
+    $offcanvasNavSubMenu = $offcanvasNav.find('.sub-menu');
+  $offcanvasNavSubMenu
+    .parent()
+    .prepend(
+      '<span class="menu-expand"><i class="fa fa-angle-down"></i></span>'
+    );
+
+  $offcanvasNavSubMenu.slideUp();
+
+  $offcanvasNav.on('click', 'li a,li .menu-expand', function (e) {
+    let $this = $(this);
+    if ($this
+      .parent()
+      .attr('class')
+      .match(/\b('menu-item-has-children'|'has-children'|'has-sub-menu')\b/) &&
+      ($this.attr('herf') === '#' || $this.hasClass('menu-expand'))
+    ) {
+      e.preventDefault();
+      if ($this.siblings('ul:visible').length) {
+        $this.siblings('ul').slideUp('slow');
+      } else {
+        $this.closet('li').siblings('li').find('ul:visible').slideUp('slow');
+        $this.siblings('ul').slideDown('slow');
+      }
+    }
+    if (
+      $this.is('a') ||
+      $this.is('span') ||
+      $this.attr('class').match(/\b('menu-item-has-children')\b/)
+    ) {
+      $this.toggleClass('menu-open');
+    }
+  });
+});
